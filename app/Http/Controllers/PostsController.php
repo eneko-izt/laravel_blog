645388836d68e33496532bc8d1ef8900d7af675f
eneko-izt <eneko@izt.coop>
2025-04-15 14:10:26 +0200

    Created Post Model and migration file for posts table

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,18 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use DB;
+use App\Post;
 
 class PostsController extends Controller
 {
     public function show($slug)
     {
-        $post = DB::table('posts')-> where('slug', $slug)->first();
-
-        //dd($post);
-
-        if (!$post) abort(404);
-    
+        $post = Post::where('slug', $slug)->firstOrFail();
+   
         return view('post', [
             'post' => $post->body
         ]);
