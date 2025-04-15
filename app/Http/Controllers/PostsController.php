33345cbdb604a44d4ca1b10987fb331b5c44ad53
eneko-izt <eneko@izt.coop>
2025-04-15 12:45:15 +0200

    Use a database to retrieve  blog body

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,22 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class PostsController extends Controller
 {
-    public function show($post)
+    public function show($slug)
     {
-        $posts = [
-            'my-first-post' => 'Hello, this is my first blog post',
-            'my-second-post' => 'Now I am getting somewhere with this code'
-        ];
-    
-        if (!array_key_exists($post, $posts)) {
-            abort(404, 'Sorry, that post was not found');
-        }
+        $post = DB::table('posts')-> where('slug', $slug)->first();
+
+        //dd($post);
+
+        if (!$post) abort(404);
     
         return view('post', [
-            'post' => $posts[$post]
+            'post' => $post->body
         ]);
     }
 }
